0d5b6b725b9bae75b023f343437f702304fcfa32
AlonsoPerezDiego <Dialpe22@gmail.com>
2017-03-07 19:10:50 +0100

    Input class commit

---
+++ src/inputs/Input.java
@@ -0,0 +1,22 @@
+/*
+ * This package is for Input methods
+ */
+package inputs;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adry and Diego
+ */
+public class Input {
+    public static String inputString(String s){
+        return JOptionPane.showInputDialog(s);
+    }
+    public static int inputInt(int s){
+        return Integer.parseInt(JOptionPane.showInputDialog(s));
+    }
+    public static float inputFloat(float s){
+       return Float.parseFloat(JOptionPane.showInputDialog(s));
+    }
+}
